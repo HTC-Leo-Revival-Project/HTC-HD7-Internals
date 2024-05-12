bcb68895f43758967ce569bb8a86492ee0dd64c7
J0SH1X <aljoshua.hell@gmail.com>
2024-05-12 21:52:53 +0200

    Update makenbh.bat

--- tools/makenbh.bat
+++ tools/makenbh.bat
@@ -6,7 +6,11 @@ del lk.bin
 echo "Please copy bootpayload.bin to this directory and press enter"
 pause
 move lk.bin bootpayload.bin
+rem replace NK.bin with our bootpayload
 .\BINInsert.exe -N .\bootpayload.bin .\os.nb.old bootshim.nk.new
+rem replace the OS image in IMGFS with just one zero to make the nbh smaller
 .\BINInsert.exe -I .\zeroos.bin .\bootshim.nk.new os.nb
+rem create the unsigned nbh hspl is needed to flash it
 .\nbhTool.exe .\hd7.txt
+rem start the installer
 .\HD7_RUUWrapper_00.exe
